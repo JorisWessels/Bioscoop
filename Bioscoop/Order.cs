e55dc3dd682ed494409b4b7b36dc1227e29b0abb
Elco Mussert <elcomussert@gmail.com>
2022-02-03 17:04:28 +0100

    Added the calculatePrice Function

--- Bioscoop/Order.cs
+++ Bioscoop/Order.cs
@@ -24,7 +24,7 @@
             _movieTicketList.Add(ticket);
         }
 
-        private bool isCorrectDay()
+        private bool isCorrectStatus()
         {
             int getTicketDay = ((int)_movieTicketList[0].getDateAndTime().DayOfWeek);
             if (!_isStudentOrder && getTicketDay == 1 || getTicketDay == 2 || getTicketDay == 3 || getTicketDay == 4)
@@ -42,23 +42,46 @@
         
         public double calculatePrice()
         {
+            double totalPrice = 0;
 
-            if (_isStudentOrder)
-            {
-                double freeTickets = Math.Floor((double)_movieTicketList.Count / 2);
-            }
-            else
-            {
-                foreach (MovieTicket ticket in _movieTicketList) { 
-                }
-            }
-          
+            // Kan makkelijker??
+            var ticketCount = 0;
 
             foreach (MovieTicket movieticket in _movieTicketList)
             {
-                
+                ticketCount++;
+                var priceTicket = movieticket.getPrice();
+                // Prijs voor ticket uitrekenen met/zonder premium
+                if (movieticket.IsPremiumTicket())
+                {
+                    if (_isStudentOrder)
+                    {
+                        priceTicket += 2.0;
+                    } else
+                    {
+                        priceTicket += 3.0;
+                    }
+                }
+
+
+                // Checken of ticket 2de is of niet (Met correcte voorwaarden)
+                if(!isCorrectStatus() || (ticketCount % 2) != 0)
+                {
+                    totalPrice += priceTicket;
+                }
+
+
             }
-            return 0;
+
+            // Eventuele korting voor niet studenten (Met correcte voorwaarden)
+            if(!isCorrectStatus() && _movieTicketList.Count >= 6)
+            {
+                totalPrice *= 0.9;
+            }
+
+
+            // Return prijs met alle kortingen inzich
+            return totalPrice;
         }
 
         public void export(TicketExportFormat exportFormat)
